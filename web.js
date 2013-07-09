536c8766b7fe775e276ed9d1495f65f0fc49535b
Pablo Gutierrez <pablojaviergutierrez@gmail.com>
2013-07-10 00:46:22 +0200

    Changed the message

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var cadena = fs.readFile('index.html',encoding);
+  var cadena = fs.readFileSync('index.html','utf8');
   response.send(cadena);
 });
 

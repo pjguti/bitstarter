6c9c7b24d2ff19dd5861a6037083c224df4ad234
Pablo Gutierrez <pablojaviergutierrez@gmail.com>
2013-07-10 00:30:14 +0200

    Changed the message

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var cadena = fs.readFile('index.html',encoding);
+  response.send(cadena);
 });
 
 var port = process.env.PORT || 5000;
